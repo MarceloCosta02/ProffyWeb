2f5ff4678d0846da54642402686a75ed6be2d40c
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-08-06 12:47:36 -0300

    Adicionando API de produção no heroku

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    // baseURL: 'http://localhost:3333' url teste
+    baseURL: 'https://proffy-nlw.herokuapp.com' // url produção
 })
 
 export default api;
